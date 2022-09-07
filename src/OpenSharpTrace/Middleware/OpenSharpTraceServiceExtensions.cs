a7aca3da656f5cbc952afc36e8f0ca7e58d81315
Francesco Del Re <francesco.delre@protonmail.com>
2022-09-07 21:14:44 +0200

    add EnvironmentVariables injection | more register options

--- src/OpenSharpTrace/Middleware/OpenSharpTraceServiceExtensions.cs
+++ src/OpenSharpTrace/Middleware/OpenSharpTraceServiceExtensions.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OpenSharpTrace.Abstractions.Persistence;
 using OpenSharpTrace.Persistence.SQL;
+using System.IO;
 
 namespace OpenSharpTrace.Middleware
 {
@@ -17,13 +18,64 @@ namespace OpenSharpTrace.Middleware
         public static void RegisterOpenSharpTrace(this IServiceCollection collection)
         {
             var configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
                .AddJsonFile("appsettings.json", true, true)
+               .AddEnvironmentVariables()
                .Build();
 
             collection.AddScoped<ISqlTraceRepository, SqlTraceRepository>();
             collection.AddDbContext<TraceContext>(options =>
             {
                 options.UseSqlServer(configuration.GetConnectionString("TraceDb"),
+                 sqlServerOptionsAction: sqlOptions =>
+                 {
+                     sqlOptions.EnableRetryOnFailure();
+                 });
+            });
+        }
+
+        /// <summary>
+        /// Register the OpenSharpTrace middleware to enable traces
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="connectionKey">Key of the connection string for trace persistence</param>
+        public static void RegisterOpenSharpTrace(this IServiceCollection collection, string connectionKey)
+        {
+            var configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile("appsettings.json", true, true)
+               .AddEnvironmentVariables()
+               .Build();
+
+            collection.AddScoped<ISqlTraceRepository, SqlTraceRepository>();
+            collection.AddDbContext<TraceContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString(connectionKey),
+                 sqlServerOptionsAction: sqlOptions =>
+                 {
+                     sqlOptions.EnableRetryOnFailure();
+                 });
+            });
+        }
+
+        /// <summary>
+        /// Register the OpenSharpTrace middleware to enable traces
+        /// </summary>
+        /// <param name="collection"></param>
+        /// <param name="connectionKey">Key of the connection string for trace persistence</param>
+        /// <param name="jsonFileName">JSON file configuration name</param>
+        public static void RegisterOpenSharpTrace(this IServiceCollection collection, string connectionKey, string jsonFileName)
+        {
+            var configuration = new ConfigurationBuilder()
+               .SetBasePath(Directory.GetCurrentDirectory())
+               .AddJsonFile(jsonFileName, true, true)
+               .AddEnvironmentVariables()
+               .Build();
+
+            collection.AddScoped<ISqlTraceRepository, SqlTraceRepository>();
+            collection.AddDbContext<TraceContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString(connectionKey),
                  sqlServerOptionsAction: sqlOptions =>
                  {
                      sqlOptions.EnableRetryOnFailure();
