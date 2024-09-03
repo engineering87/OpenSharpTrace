04fdaba664e0d63dbf5ff871b65633bec703a5e3
Francesco Del Re <francesco.delre.87@gmail.com>
2024-09-03 18:36:03 +0200

    Add EnsureTraceTableExists

--- src/OpenSharpTrace/Persistence/SQL/TraceContext.cs
+++ src/OpenSharpTrace/Persistence/SQL/TraceContext.cs
@@ -18,11 +18,12 @@ namespace OpenSharpTrace.Persistence.SQL
             try
             {
                 Database.EnsureCreated();
+
+                EnsureTraceTableExists();
             }
             catch
             {
                 // database is not ready or the connectionstring is wrong
-                // ignore
             }
         }
 
@@ -31,6 +32,49 @@ namespace OpenSharpTrace.Persistence.SQL
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+        }
+
+        private void EnsureTraceTableExists()
+        {
+            using (var connection = Database.GetDbConnection())
+            {
+                try
+                {
+                    connection.Open();
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = @"
+                            IF NOT EXISTS (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'Trace')
+                            BEGIN
+                                CREATE TABLE [dbo].[Trace](
+	                                [Id] [bigint] IDENTITY(1,1) NOT NULL,
+	                                [TransactionId] [nvarchar](36) NULL,
+	                                [ServerId] [nvarchar](MAX) NULL,
+	                                [ClientId] [nvarchar](MAX) NULL,
+	                                [HttpMethod] [nvarchar](7) NULL,
+	                                [HttpPath] [nvarchar](MAX) NULL,
+	                                [HttpStatusCode] [int] NULL,
+	                                [ActionDescriptor] [nvarchar](MAX) NULL,
+	                                [RemoteAddress] [nvarchar](MAX) NULL,
+	                                [JsonRequest] [nvarchar](MAX) NULL,
+	                                [JsonResponse] [nvarchar](MAX) NULL,
+	                                [TimeStamp] [datetime2](7) NULL,
+	                                [Exception] [nvarchar](MAX) NULL,
+	                                [ExecutionTime] [numeric] NULL,
+                                ) ON [PRIMARY];
+                            END";
+                        command.ExecuteNonQuery();
+                    }
+                }
+                catch
+                {
+                    // database is not ready or the connectionstring is wrong
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
         }
     }
 }
